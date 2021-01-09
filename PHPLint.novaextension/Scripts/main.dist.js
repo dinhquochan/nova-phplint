7b775a194a4f764cf558746b262975b3a2e73c58
Dinh Quoc Han <contact@dinhquochan.com>
2021-01-09 21:37:17 +0700

    first

---
+++ PHPLint.novaextension/Scripts/main.dist.js
@@ -0,0 +1,132 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var activate = function() {
+  // Do work when the extension is activated.
+};
+
+var deactivate = function() {
+  // Clean up state before the extension is deactivated
+};
+
+const execute = (path,  options) => {
+  return new Promise((resolve) => {
+    const process = new Process(path, options);
+
+    // Copy all stdout into an array of lines.
+    const stdout = [];
+  
+    process.onStdout((line) => stdout.push(line));
+
+    // Copy all stderr into an array of lines.
+    const stderr = [];
+    process.onStderr((line) => stderr.push(line));
+
+    // Resolve the promise once the process has exited,
+    // with the stdout and stderr as single strings and the status code number.
+    process.onDidExit((status) =>
+      resolve({
+        status,
+        stderr: stderr.join("\n"),
+        stdout: stdout.join("\n"),
+      })
+    );
+
+    // Start the process.
+    process.start();
+  })
+};
+
+class IssuesProvider {
+  constructor() {
+    //
+  }
+  
+  async findPHPPath() {
+    const { stdout, status } = await execute("/usr/bin/env", {
+      args: ["which", "php"],
+    });
+    
+    return status === 0 ? stdout.trim() : null
+  }
+  
+  async resolvePHPPath() {
+    const execPathFromWorkspaceConfig = nova.workspace.config.get('DinhQuocHan.PHPLint.execPath');
+    const execPathFromConfig = (execPathFromWorkspaceConfig
+      ? execPathFromWorkspaceConfig
+      : nova.config.get('DinhQuocHan.PHPLint.execPath'));
+    
+    if (execPathFromConfig) {
+      this.phpPath = execPathFromConfig;
+      return
+    }
+    
+    try {
+      this.phpPath = await this.findPHPPath();
+    
+      if (! this.phpPath) {
+        throw new Error('PHP not installed on your $PATH.')
+      }
+    } catch (error) {
+      console.error('Failed to find php path.');
+      console.error(error);
+    }
+  }
+  
+  async provideIssues(editor) {
+    await this.resolvePHPPath();
+    
+    const issues = [];
+    const filePath = editor.document.path;
+    const phpPath = this.phpPath;
+
+    const execOptions = {
+      args: [
+        phpPath,
+        '-l',
+        '-n',
+        '-d',
+        'display_errors=On',
+        '-d',
+        'log_errors=Off',
+        filePath
+      ]
+    };
+    const { stdout, status } = await execute('/usr/bin/env', execOptions);
+    
+    const searchError = stdout.search('No syntax errors');
+    
+    if (searchError !== -1) {
+      return issues
+    }
+    
+    const regex = /^(?:Parse|Fatal) (error):(\s*(parse|syntax) error,?)?\s*((?:unexpected \'([^\']+)\')?.*) (?:in - )?on line (\d+)/gm;
+    const found = regex.exec(stdout);
+  
+    if (found[0] || false) {
+      const issue = new Issue();
+      const type = found[3];
+      const message = found[4].replace(filePath, nova.workspace.relativizePath(filePath));
+      issue.message = `Error (${type}): ${message}`;
+      issue.severity = IssueSeverity.Error;
+      issue.line = parseInt(found[6]);
+      issue.column = 0;
+
+      issues.push(issue);
+    }
+    
+    return issues
+  }
+}
+
+nova.assistants.registerIssueAssistant('php', new IssuesProvider());
+
+var main = {
+	activate: activate,
+	deactivate: deactivate
+};
+
+exports.activate = activate;
+exports.deactivate = deactivate;
+exports.default = main;
---
+++ README.md
@@ -0,0 +1,27 @@
+# PHPLint for Nova
+
+**PHPLint** automatically lints all open files, then reports errors and warnings in Nova's **Issues** sidebar and the editor gutter:
+
+
+![](https://nova.app/images/en/dark/editor.png)
+
+## Linter Installation
+### Requirements
+
+PHPLint requires PHP installed on your Mac:
+
+1. Install [Homebrew](https://brew.sh).
+2. Run `brew install php`
+
+### Installation
+
+1. Open **Nova**.
+2. Open **Extensions → Extension Library...**
+3. Search extension PHPLint (Dinh Quoc Han)
+4. Click **Install**.
+
+### Configuration
+
+To configure global preferences, open **Extensions → Extension Library...** then select PHPLint's **Preferences** tab.
+
+You can also configure preferences on a per-project basis in **Project → Project Settings...**
